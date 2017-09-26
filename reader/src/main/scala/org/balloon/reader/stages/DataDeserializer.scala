037ca6e5143299891e00403048b3e60888412c02
Piotr Kacprzak <piotr.kacprzak@workday.com>
2017-09-26 23:26:02 +0100

    data deserializer stage

---
+++ reader/src/main/scala/org/balloon/reader/stages/DataDeserializer.scala
@@ -0,0 +1,30 @@
+package org.balloon.reader.stages
+
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import org.balloon.data.observatory.ObservatoryData
+
+class DataDeserializer extends GraphStage[FlowShape[String, ObservatoryData]] {
+  val input: Inlet[String] = Inlet[String]("line.in")
+  val output: Outlet[ObservatoryData] = Outlet[ObservatoryData]("data.out")
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+    setHandler(input, new InHandler {
+      override def onPush(): Unit = {
+        val line: String = grab(input)
+        val data: Option[ObservatoryData] = ObservatoryData.from(line)
+        if (data.isDefined) {
+          push(output, data.get)
+        } else {
+          pull(input)
+        }
+      }
+    })
+
+    setHandler(output, new OutHandler {
+      override def onPull(): Unit = pull(input)
+    })
+  }
+
+  override def shape: FlowShape[String, ObservatoryData] = FlowShape(input, output)
+}
---
+++ reader/src/test/scala/org/balloon/reader/stages/DataDeserializerTest.scala
@@ -0,0 +1,43 @@
+package org.balloon.reader.stages
+
+import java.time.LocalDateTime
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.testkit.TestKit
+import org.balloon.data.observatory.{Australia, ObservatoryData}
+import org.balloon.data.temperature.Celsius
+import org.scalatest._
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class DataDeserializerTest extends TestKit(ActorSystem("DataDeserializerTestSystem")) with FreeSpecLike with Matchers
+  with BeforeAndAfterAll {
+
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  "When data" - {
+    "is valid deserialize observatory data" in {
+      val source = Source.single("2014-10-29T13:34:21|10|AU")
+      val sink = Sink.head[ObservatoryData]
+      val flow: Flow[String, ObservatoryData, NotUsed] = Flow[String].via(new DataDeserializer)
+      val f: Future[ObservatoryData] = source.via(flow).runWith(sink)
+
+      val data = Await.result(f, 2 seconds)
+      data should equal(Australia(LocalDateTime.parse("2014-10-29T13:34:21"), Celsius(10)))
+    }
+
+    "is invalid deserializer omits it" in {
+      val source = Source(List("2014-10-29T13:34|10|AU", "2014-10-29T13:34:21|10|AU"))
+      val flow: Flow[String, ObservatoryData, NotUsed] = Flow[String].via(new DataDeserializer)
+      val f: Future[List[ObservatoryData]] = source.via(flow).runFold(List[ObservatoryData]())((a, b) => b :: a)
+
+      val data = Await.result(f, 2 seconds)
+      data should equal(List(Australia(LocalDateTime.parse("2014-10-29T13:34:21"), Celsius(10))))
+    }
+  }
+
+}
